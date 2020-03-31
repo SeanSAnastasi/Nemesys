61e4cb160a32d19154fcb4c5dc7fdc90148279e6
SeanSAnastasi <sean.s.anastasi@gmail.com>
2020-03-31 19:33:58 +0200

    Create database

---
+++ Models/NemesysDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Nemesys.Models
+{
+    public class NemesysDBContext:DbContext
+    {
+        public NemesysDBContext()
+        {
+
+        }
+        public DbSet<InvestigationModel> InvestigationModel { get; set; }
+        public DbSet<InvestigatorModel> InvestigatorModel { get; set; }
+        public DbSet<ReporterModel> ReporterModel { get; set; }
+
+    }
+}
