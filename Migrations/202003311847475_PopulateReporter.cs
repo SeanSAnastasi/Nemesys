756fe10e44c0ea2b92b2dd40915a6f23dc39ec17
SeanSAnastasi <sean.s.anastasi@gmail.com>
2020-03-31 20:57:36 +0200

    Create Database and populate with test value

---
+++ Migrations/202003311847475_PopulateReporter.cs
@@ -0,0 +1,17 @@
+﻿namespace Nemesys.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateReporter : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Reporters(Email,Password,Username,ActiveReports,HandledReports,PendingReports,ProfileImage) VALUES ('john@example.com','password123','John',0,0,0,0)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Models/Investigation.cs
+++ Models/Investigation.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace Nemesys.Models
 {
-    public class InvestigationModel
+    public class Investigation
     {
         public int Id { get; set; }
         public int ReporterId { get; set; }
-
+        public DateTime StartDate { get; set; }
         public int ReportId { get; set; }
         public int InvestigatorId { get; set; }
     }
