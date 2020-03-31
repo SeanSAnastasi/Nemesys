0fd4828d1412910652b175973bc8af10656191d3
SeanSAnastasi <sean.s.anastasi@gmail.com>
2020-03-31 21:28:21 +0200

    Add data notations

--- Models/Investigator.cs
+++ Models/Investigator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace Nemesys.Models
     public class Investigator
     {
         public int Id { get; set; }
+        [Required]
         public String Email { get; set; }
+        [Required]
         public String Password { get; set; }
+        [Required]
         public String Username { get; set; }
         public int ActiveInvestigations { get; set; }
         public int TotalInvestigations { get; set; }
