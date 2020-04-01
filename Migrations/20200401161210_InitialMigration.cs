779162e9e45b1631f00009387ba19cf5af6b0a55
SeanSAnastasi <sean.s.anastasi@gmail.com>
2020-04-01 18:34:59 +0200

    Change database to EF core and finish investigations page

---
+++ Migrations/20200401161210_InitialMigration.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nemesys.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Investigator",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(nullable: false),
+                    ActiveInvestigations = table.Column<int>(nullable: false),
+                    TotalInvestigations = table.Column<int>(nullable: false),
+                    ProfileImage = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investigator", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reporter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(nullable: false),
+                    ActiveReports = table.Column<int>(nullable: false),
+                    HandledReports = table.Column<int>(nullable: false),
+                    PendingReports = table.Column<int>(nullable: false),
+                    ProfileImage = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reporter", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReporterId = table.Column<int>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ImageLocation = table.Column<byte[]>(nullable: true),
+                    Likes = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Report_Reporter_ReporterId",
+                        column: x => x.ReporterId,
+                        principalTable: "Reporter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investigation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReporterId = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    ReportId = table.Column<int>(nullable: true),
+                    InvestigatorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investigation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Investigation_Investigator_InvestigatorId",
+                        column: x => x.InvestigatorId,
+                        principalTable: "Investigator",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Investigation_Report_ReportId",
+                        column: x => x.ReportId,
+                        principalTable: "Report",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Investigation_Reporter_ReporterId",
+                        column: x => x.ReporterId,
+                        principalTable: "Reporter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investigation_InvestigatorId",
+                table: "Investigation",
+                column: "InvestigatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investigation_ReportId",
+                table: "Investigation",
+                column: "ReportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investigation_ReporterId",
+                table: "Investigation",
+                column: "ReporterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_ReporterId",
+                table: "Report",
+                column: "ReporterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Investigation");
+
+            migrationBuilder.DropTable(
+                name: "Investigator");
+
+            migrationBuilder.DropTable(
+                name: "Report");
+
+            migrationBuilder.DropTable(
+                name: "Reporter");
+        }
+    }
+}
