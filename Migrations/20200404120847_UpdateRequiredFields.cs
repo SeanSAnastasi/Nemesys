3e9f864363af3eff393378907727b7f675149d36
SeanSAnastasi <sean.s.anastasi@gmail.com>
2020-04-04 14:24:40 +0200

    Try to fix report details bug

---
+++ Migrations/20200404120847_UpdateRequiredFields.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nemesys.Migrations
+{
+    public partial class UpdateRequiredFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Report_Reporter_ReporterId",
+                table: "Report");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReporterId",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HazardType",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Report_Reporter_ReporterId",
+                table: "Report",
+                column: "ReporterId",
+                principalTable: "Reporter",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Report_Reporter_ReporterId",
+                table: "Report");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Report",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Report",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReporterId",
+                table: "Report",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HazardType",
+                table: "Report",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Report_Reporter_ReporterId",
+                table: "Report",
+                column: "ReporterId",
+                principalTable: "Reporter",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Models/Report.cs
+++ Models/Report.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,15 @@ namespace Nemesys.Models
     public class Report
     {
         public int Id { get; set; }
+        [Required]
         public Reporter Reporter { get; set; }
-
+        [Required]
         public String Title { get; set; }
+        [Required]
         public DateTime Date { get; set; }
-        
+        [Required]
         public String HazardType { get; set; }
-
+        [Required]
         public String Status { get; set; }
         public String Location { get; set; }
         public String Description { get; set; }
