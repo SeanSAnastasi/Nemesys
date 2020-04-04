f3782ba35cfaabf1266b57a72af3645258bd0962
Azzopardi <58291238+Azz0pardi@users.noreply.github.com>
2020-04-04 12:21:08 +0200

    Report alteration

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Nemesys.Models;
 
 namespace Nemesys.Controllers
 {
     public class ReportController : Controller
     {
+        private NemesysDBContext _context;
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ReportController(NemesysDBContext context)
+        {
+            _context = context;
+
+        }
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Title = "Report";
+            var report = _context.Report.ToList();
+            
+            return View(report);
         }
     }
 }
---
+++ Migrations/20200404101254_danielRecenti.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nemesys.Migrations
+{
+    public partial class danielRecenti : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HazardType",
+                table: "Report",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HazardType",
+                table: "Report");
+        }
+    }
+}
