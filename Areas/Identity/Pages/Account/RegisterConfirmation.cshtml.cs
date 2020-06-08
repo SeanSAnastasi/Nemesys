c39a123867824644aa61540af723eaa5b9ea7a79
SeanSAnastasi <sean.s.anastasi@gmail.com>
2020-06-09 01:58:39 +0200

    Fix email and search bugs

---
+++ Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Nemesys.Areas.Identity.Data;
+using Nemesys.Services;
+
+namespace Nemesys.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterConfirmationModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMailService _sender;
+
+        public RegisterConfirmationModel(UserManager<User> userManager, IMailService sender)
+        {
+            _userManager = userManager;
+            _sender = sender;
+        }
+
+        public string Email { get; set; }
+
+        public bool DisplayConfirmAccountLink { get; set; }
+
+        public string EmailConfirmationUrl { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string email)
+        {
+            if (email == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with email '{email}'.");
+            }
+
+            Email = email;
+            
+            
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                EmailConfirmationUrl = Url.Page(
+                    "/Account/ConfirmEmail",
+                    pageHandler: null,
+                    values: new { area = "Identity", userId = userId, code = code },
+                    protocol: Request.Scheme);
+            
+            await _sender.SendEmailAsync(Email, "Confirm your email",$"Please confirm your account by <a href='{EmailConfirmationUrl}'>clicking here</a>.");
+
+            return Page();
+        }
+    }
+}
